d3fe765ecea33f4aab0b69be5efe54992bb5024f
Sergey Rakov <rakovsergey94@gmail.com>
2020-10-29 10:57:36 +0300

    Chapter 2-> done

---
+++ src/main/java/com/tacocloud/model/Design.java
@@ -0,0 +1,7 @@
+package com.tacocloud.model;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Design {
+}
---
+++ src/main/java/com/tacocloud/model/Taco.java
@@ -0,0 +1,20 @@
+package com.tacocloud.model;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Data
+@Component
+public class Taco {
+
+    @NotNull
+    @Size(min = 5, message = "Name must be at least 5 characters long")
+    private String name;
+
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private List<String> ingredients;
+}
